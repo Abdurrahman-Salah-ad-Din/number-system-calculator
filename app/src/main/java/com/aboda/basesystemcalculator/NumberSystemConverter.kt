4845e2bd980cf1320deed4224289f7da819744c6
Abdurrahman <abdosalahas.01128@gmail.com>
2023-03-13 23:36:00 +0200

    add 4 methods the decimalToOct method,  binaryToDecimal method, octToDecimal method and hexToDecimal method

--- app/src/main/java/com/aboda/basesystemcalculator/NumberSystemConverter.kt
+++ app/src/main/java/com/aboda/basesystemcalculator/NumberSystemConverter.kt
@@ -1,5 +1,7 @@
 package com.aboda.basesystemcalculator
 
+import kotlin.math.pow
+
 class NumberSystemConverter {
 
     fun decimalToBin(decimalValue: String): String {
@@ -16,5 +18,43 @@ class NumberSystemConverter {
         return java.lang.Long.toHexString(decimalValue.toLong())
     }
 
+    fun decimalToOct(decimalValue: String): String {
+        var longDecimalValue = decimalValue.toLong()
+        var myOctValue  = ""
+        while (longDecimalValue > 0) {
+            myOctValue = (longDecimalValue % 8).toString().plus(myOctValue)
+            longDecimalValue /= 8
+        }
+        return myOctValue
+    }
 
+    fun binaryToDecimal(binaryValue: String): String {
+        var longDecimalValue = 0L
+        var i = 0.0
+        var myBinaryValue = binaryValue
+        while (myBinaryValue.length > 0) {
+            longDecimalValue += (2.0.pow(i).toLong() *
+                    myBinaryValue.substring(myBinaryValue.length - 1).toLong())
+            myBinaryValue = myBinaryValue.substring(0, myBinaryValue.length - 1)
+            i++
+        }
+        return longDecimalValue.toString()
+    }
+
+    fun octToDecimal(octValue: String): String {
+        var longDecimalValue = 0L
+        var i = 0.0
+        var myOctValue = octValue
+        while (myOctValue.length > 0) {
+            longDecimalValue += (8.0.pow(i).toLong() *
+                    myOctValue.substring(myOctValue.length - 1).toLong())
+            myOctValue = myOctValue.substring(0, myOctValue.length - 1)
+            i++
+        }
+        return longDecimalValue.toString()
+    }
+
+    fun hexToDecimal(hexValue: String): String {
+        return hexValue.toLong(16).toString()
+    }
 }
